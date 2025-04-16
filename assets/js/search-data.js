69c219809338c86a42dcea97b66b8877bed7aaab
danielearwicker <dan@earwicker.com>
2025-04-16 07:11:59 +0000

    Deploying to gh-pages from @ danielearwicker/danielearwicker.github.io@0019c5bd11bd8dc12fe6456e0c5686396d54508d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "post-poorly-structured-notes-on-ai-part-2",
+        },{id: "post-poorly-structured-notes-on-ai-part-3",
+        
+          title: "Poorly Structured Notes on AI Part 3",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/ai3/";
+          
+        },
+      },{id: "post-poorly-structured-notes-on-ai-part-2",
         
           title: "Poorly Structured Notes on AI Part 2",
         
